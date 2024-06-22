3bb28342be47bd58fab65852eb4fc0dc00d2fffb
Hubert Chen <01@trle5.xyz>
2024-06-22 22:29:00 +0800

    custom style
    remove container border
    move site info and urls in header
    disable hero and post components
    some small style change

--- src/_components/container.tsx
+++ src/_components/container.tsx
@@ -3,7 +3,7 @@ export type Props = {
 }
 
 export default ({ comp: { Footer, Header }, children, className }: Lume.Data & Props) => (
-  <main class={`${className ?? ''} w-full max-w-screen-md min-h-screen mx-auto p-8 flex flex-col gap-8 md:border-x border-outline-variant`}>
+  <main class={`${className ?? ''} w-full max-w-screen-md 2xl:max-w-screen-lg min-h-screen mx-auto p-8 flex flex-col gap-8`}>
     <Header />
     {children}
     <Footer />
