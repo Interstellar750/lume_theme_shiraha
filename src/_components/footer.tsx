ac9fe8905c65f74c0915c42f5e10e9072d1f5b61
Hubert Chen <01@trle5.xyz>
2024-06-22 22:29:00 +0800

    custom style
    remove container border
    move site info and urls in header
    disable hero and post components
    some small style change

--- src/_components/footer.tsx
+++ src/_components/footer.tsx
@@ -1,10 +1,14 @@
-export default ({ lts }: Lume.Data, {}: Lume.Helpers) => (
+export default ({ lts }: Lume.Data, { url }: Lume.Helpers) => (
   <footer class="h-12 mx-auto flex gap-1 place-items-center motion-safe:view-transition-footer">
-    <span>© {new Date().getUTCFullYear()} {lts.author.name}</span>
-    <span> · </span>
-    <span>Powered by</span>
-    <a href="https://github.com/importantimport/lume_theme_shiraha">Shiraha</a>
-    <span>&</span>
-    <a href="https://lume.land">Lume</a>
+    <div>
+      <span>© {new Date().getUTCFullYear()}</span>
+      <a href={url('/')}>{lts.author.name}</a>
+      <span class="md-dot">·</span>
+      <br class="sm-bk-line" />
+      <span>Powered by</span>
+      <a href="https://github.com/importantimport/lume_theme_shiraha" target="_blank">Shiraha</a>
+      <span>&</span>
+      <a href="https://lume.land" target="_blank">Lume</a>
+    </div>
   </footer>
 )
